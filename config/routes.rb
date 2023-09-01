6651228f4f36579d173ef99913bf0fc3611f47cb
micaela dauria schmidt <micaelaschmidt47@gmail.com>
2023-09-01 10:56:55 -0300

    Create Controllers and Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'properties/index'
+  get 'properties/show'
+  get 'properties/new'
+  get 'properties/create'
+  get 'properties/edit'
+  get 'properties/update'
+  get 'properties/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
