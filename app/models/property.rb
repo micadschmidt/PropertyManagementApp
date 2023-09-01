99ee4cc2b20bc349dfc477b4086c6e1c32474f6e
micaela dauria schmidt <micaelaschmidt47@gmail.com>
2023-09-01 10:47:16 -0300

    create model validations

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,12 @@
 class Property < ApplicationRecord
+  belongs_to :user
+  has_many_attached :photos
+
+  validates :property_type, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :address, presence: true
+  validates :area, numericality: { greater_than: 0 }
+  validates :rooms, numericality: { greater_than_or_equal_to: 0 }
+  validates :bathrooms, numericality: { greater_than_or_equal_to: 0 }
+  validates :photos, presence: true
 end
